47428091f05e2002f1827385e6dfd4d35b230fe0
Ana Luiza Motta <analuizamtg@gmail.com>
2018-03-08 19:11:39 -0300

    Add generated URL display

--- react/components/Debugger.js
+++ react/components/Debugger.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { getOrderForm } from '../actions/index'
+import { generateUrl } from '../utils'
 
 class Debugger extends Component {
+
+  componentDidMount(){
+    if (!(window.vtexjs && window.vtexjs.checkout && window.vtexjs.checkout.orderForm)){
+      this.props.getOrderForm(this.context.account)
+    }
+  }
+
   render() {
+    if (!window.vtexjs && !this.props.orderForm){
+      return (
+        <h2> Loading </h2>
+      )
+    }
+    const generatedUrl = generateUrl((window.vtexjs && window.vtexjs.checkout && window.vtexjs.checkout.orderForm) || this.props.orderForm, this.context.account)
+
     return (
-      <h3> Hello!</h3>
+      <div>
+        <h3> Hello!</h3>
+        {generatedUrl}
+      </div>
     )
   }
 }
 
 Debugger.propTypes = {
+  getOrderForm: PropTypes.func.isRequired
 }
 
-export default Debugger
+Debugger.contextTypes = {
+  account: PropTypes.string
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  orderForm: state.orderForm
+})
+
+export default connect(mapStateToProps, {
+  getOrderForm
+})(Debugger)
