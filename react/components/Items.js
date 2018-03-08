1900abe0b3f8fb03c6b6ce1bfe3eae2f7e3e40d8
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2018-03-08 19:52:57 -0300

    Create Items page

--- react/components/Items.js
+++ react/components/Items.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Label from './Label'
+import Input from '@vtex/styleguide/lib/Input'
+import Button from '@vtex/styleguide/lib/Button'
 
 class Items extends Component {
   render() {
     return (
-      <h3>Items</h3>
+      <div className="ph4">
+        <p>
+          <Label htmlFor="itemsQuantity">Number of items</Label>
+          <Input htmlProps={{ id: 'itemsQuantity' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsCategories">Categories</Label>
+          <Input htmlProps={{ id: 'itemsCategories' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsBrands">Brands</Label>
+          <Input htmlProps={{ id: 'itemsBrands' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsCollections">Collections</Label>
+          <Input htmlProps={{ id: 'itemsCollections' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsSellers">Sellers</Label>
+          <Input htmlProps={{ id: 'itemsSellers' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsIDs">SKU IDs</Label>
+          <Input htmlProps={{ id: 'itemsIDs' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsPriceFrom">Price from</Label>
+          <Input htmlProps={{ id: 'itemsPriceFrom' }} />
+        </p>
+        <p>
+          <Label htmlFor="itemsPriceUpTo">Price up to</Label>
+          <Input htmlProps={{ id: 'itemsPriceUpTo' }} />
+        </p>
+
+        <div className="tc">
+          <Button primary>Add items do Cart</Button>
+        </div>
+      </div>
     )
   }
 }
