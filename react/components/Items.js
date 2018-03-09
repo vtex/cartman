5506843b52c2b42b780474e1925b74cec3a73d82
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2018-03-09 03:45:50 -0300

    Set default number of items

--- react/components/Items.js
+++ react/components/Items.js
@@ -15,7 +15,7 @@ class Items extends Component {
   handleNumberOfItemsChange = (e) => {
     this.setState({
       ...this.state,
-      numberOfItems: e.target.value
+      numberOfItems: e.target.value === null ? 1 : e.target.value
     })
   }
 
