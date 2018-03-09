4866e77a7f9ce3f6635eaf18f3ab43c725452de9
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2018-03-08 22:28:27 -0300

    Fix back button

--- react/components/Header.js
+++ react/components/Header.js
@@ -4,8 +4,14 @@ import PropTypes from 'prop-types'
 class Header extends Component {
   render() {
     return (
-      <header className="f3 bg-light-silver serious-black pa5 tc">
-        { this.props.page !== 'home' && <span onClick={this.props.backToHome}>back</span> }
+      <header className="relative f3 bg-light-silver serious-black pa5 tc">
+        { this.props.page !== 'home' && (
+          <button onClick={this.props.backToHome} className="absolute left-0 top-0 bn bg-transparent pointer pa5">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 16 16" width="16" height="16">
+              <polyline fill="none" stroke="#111111" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="11.5,0.5 4,8 11.5,15.5 "></polyline>
+            </svg>
+          </button>
+        ) }
         { this.props.page === 'home' && <span>Cart Debugger</span>}
         { this.props.page === 'read' && <span>Read</span>}
         { this.props.page === 'items' && <span>Items</span>}
