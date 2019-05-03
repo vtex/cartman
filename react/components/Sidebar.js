b709af0bd238382993a9b22812f6de19f009539b
Augusto Barbosa <augusto.barbosa@vtex.com.br>
2019-05-03 11:57:47 -0300

    Add onda-v1 class

--- react/components/Sidebar.js
+++ react/components/Sidebar.js
@@ -164,7 +164,7 @@ class Sidebar extends Component {
     return (
       cartmanEnabled
         ? (
-          <div className="vtex-cartman">
+          <div className="vtex-cartman onda-v1">
             {
               this.state.isOpen && (
                 <div className="vtex-cartman-container fixed right-0 bottom-2-m w-100 mw6-m pl7-m pr5-m z-max">
